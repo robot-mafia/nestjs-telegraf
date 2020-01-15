6e24f136a332461fb3f3974f92293dfbce2ffa33
Aleksandr Bukhalo <aleksandr@bukhalo.com>
2020-01-15 17:26:03 +0300

    refactor: prettier config fix, remove console.log

--- lib/interfaces/handle-parameters.interface.ts
+++ lib/interfaces/handle-parameters.interface.ts
@@ -1,16 +1,16 @@
-import { ContextTransformer } from './';
-import { HearsTriggers } from 'telegraf';
-import { Type } from '@nestjs/common';
+import { ContextTransformer } from './'
+import { HearsTriggers } from 'telegraf'
+import { Type } from '@nestjs/common'
 
 interface ArgumentTransformation {
-  index: number;
-  transform: Type<ContextTransformer>;
+  index: number
+  transform: Type<ContextTransformer>
 }
 
 export interface HandleParameters {
-  onStart?: boolean;
-  command?: string;
-  message?: string | RegExp;
-  action?: HearsTriggers;
-  transformations?: ArgumentTransformation[];
+  onStart?: boolean
+  command?: string
+  message?: string | RegExp
+  action?: HearsTriggers
+  transformations?: ArgumentTransformation[]
 }
