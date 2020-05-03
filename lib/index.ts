beeffc5088f59fe54e7fd17dfa98db977d5d83f2
Aleksandr Bukhalo <abukhalo@icloud.com>
2020-05-03 10:10:16 +0300

    feat: export telegraf helpers

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,10 @@
+export * as Composer from 'telegraf/composer';
+export * as Markup from 'telegraf/markup';
+export * as BaseScene from 'telegraf/scenes/base';
+export * as session from 'telegraf/session';
+export * as Stage from 'telegraf/stage';
+export * as WizardScene from 'telegraf/scenes/wizard';
+
 export * from './telegraf.module';
 export * from './interfaces';
 export * from './decorators';
