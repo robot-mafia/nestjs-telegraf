744da0a92255c410fee0940cfc66752266e0ff5a
Aleksandr Bukhalo <a@bukhalo.com>
2020-09-13 11:28:42 +0300

    refactor: cleanup context interface

--- lib/interfaces/context.interface.ts
+++ lib/interfaces/context.interface.ts
@@ -1,11 +1,3 @@
 import { Context as TelegrafContext } from 'telegraf';
 
-export interface Context extends TelegrafContext {
-  [key: string]: any;
-}
-
-/**
- * Removed type from Telegraf v3.38.0, added for backward compatibility.
- * TODO: remove on next major release
- */
-export interface ContextMessageUpdate extends Context {}
+export interface Context extends TelegrafContext {}
