82a9c259f6e00fb1f89ca938f06ebea50d0e0dce
Morb0 <exstrominer@gmail.com>
2021-01-03 13:50:04 +0300

    feat(sample-app): add multi bot instace

--- sample/app.constants.ts
+++ sample/app.constants.ts
@@ -1,1 +1,2 @@
 export const HELLO_SCENE_ID = 'HELLO_SCENE_ID';
+export const SUPPORT_BOT_NAME = 'support';
--- sample/app.module.ts
+++ sample/app.module.ts
@@ -4,11 +4,17 @@ import { EchoService } from './echo.service';
 import { AppUpdate } from './app.update';
 import { HelloScene } from './scenes/hello.scene';
 import { sessionMiddleware } from './middleware/session.middleware';
+import { SUPPORT_BOT_NAME } from './app.constants';
 
 @Module({
   imports: [
     TelegrafModule.forRoot({
-      token: '1467731595:AAHCvH65H9VQYKF9jE-E8c2rXsQBVAYseg8', // Don't steal >:(
+      token: '1417509321:AAEHz8a2QSAP4cTHh4Z6-ulePysFaUx4SjY', // Don't steal >:(
+      middlewares: [sessionMiddleware],
+    }),
+    TelegrafModule.forRoot({
+      name: SUPPORT_BOT_NAME,
+      token: '1435922623:AAHmBmnyqroHxDbuK6OKsLV8Y_oB_Lf9E6E', // Don't steal >:(
       middlewares: [sessionMiddleware],
     }),
   ],
