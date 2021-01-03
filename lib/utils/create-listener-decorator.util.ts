e75415340ba89fa493b72adf6eeab3ab173ede66
Morb0 <exstrominer@gmail.com>
2021-01-03 15:35:18 +0300

    fix(sample-app): make possible to run app

--- lib/utils/create-listener-decorator.util.ts
+++ lib/utils/create-listener-decorator.util.ts
@@ -1,14 +1,14 @@
 import { SetMetadata } from '@nestjs/common';
-import { Composer } from 'telegraf';
-import { ComposerMethodArgs, UpdateMethods } from '../types';
+import { BaseScene as Scene } from 'telegraf';
+import { ComposerMethodArgs, SceneMethods } from '../types';
 import { UPDATE_LISTENER_METADATA } from '../telegraf.constants';
 import { ListenerMetadata } from '../interfaces';
 
-export function createUpdateListenerDecorator<Method extends UpdateMethods>(
-  method: unknown,
+export function createListenerDecorator<Method extends SceneMethods>(
+  method: Method,
 ) {
   return (
-    ...args: ComposerMethodArgs<Composer<never>, Method>
+    ...args: ComposerMethodArgs<Scene<never>, Method>
   ): MethodDecorator => {
     return SetMetadata(UPDATE_LISTENER_METADATA, {
       method,
