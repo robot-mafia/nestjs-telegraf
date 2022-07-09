295ca4ffdc72621a3a98e009ba2efd9c78e59571
Taymuraz Kaytmazov <atassikay38@gmail.com>
2022-07-09 18:53:22 +0800

    Update telegraf-all-bots.provider.ts

--- lib/telegraf-all-bots.provider.ts
+++ lib/telegraf-all-bots.provider.ts
@@ -4,8 +4,6 @@ import { TELEGRAF_ALL_BOTS } from './telegraf.constants';
 
 export const allBotsMap = new Map<string, Telegraf<any>>();
 
-setTimeout(() => console.log(allBotsMap), 2000);
-
 export const telegrafAllBotsProvider: Provider = {
   provide: TELEGRAF_ALL_BOTS,
   useValue: allBotsMap,
