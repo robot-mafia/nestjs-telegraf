a94cad7a4b7b536205cb5bfb059313bec16cbc88
0x467 <0x467@pm.me>
2024-06-26 14:03:53 +0300

    catch telegram bot api errors

--- lib/utils/create-bot-factory.util.ts
+++ lib/utils/create-bot-factory.util.ts
@@ -1,5 +1,6 @@
 import { Telegraf } from 'telegraf';
 import { TelegrafModuleOptions } from '../interfaces';
+import { Logger } from '@nestjs/common';
 
 export async function createBotFactory(
   options: TelegrafModuleOptions,
@@ -7,6 +8,9 @@ export async function createBotFactory(
   const bot = new Telegraf<any>(options.token, options.options);
 
   bot.use(...(options.middlewares ?? []));
+  bot.catch((err, ctx) =>
+    Logger.error(err, `Telegraf: ${ctx.botInfo.username}`),
+  );
 
   if (options.launchOptions !== false) {
     bot.launch(options.launchOptions);
